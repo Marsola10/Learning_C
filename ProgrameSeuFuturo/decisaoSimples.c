4024262f300bc5a2929b139c9c4fff1a8b62b0a1
Gabriel Lucca Marsola <mgabriellucca@gmail.com>
2026-06-28 16:42:16 -0300

    aula de estrutura de repetição 'For'

---
+++ ProgrameSeuFuturo/decisaoSimples.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    //Aula 45 - Estrutura de decisão simples. Verificando se um número é positivo ou negativo.
+
+    if (5 < 0) {
+        printf("\tValor negativo\n");
+    } else {
+        printf("\tValor positivo\n");
+    }
+    return 0;
+}
---
+++ ProgrameSeuFuturo/repeticaoFor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() { // Estrutura de repetição for. 
+
+int i;
+                    // i++ -> i = i + 1; // i++ incrementa o valor de i em 1 unidade.
+for(i = 1; i <= 10; i++){ // i = 1;  i = i + 1; i = 1 + 1; i = 2 + 1; incrementa sucessivamente até... -> 10
+    printf("\t%d\n", i);
+}
+
+
+
+    /*
+printf("1\n");
+printf("2\n");
+printf("3\n");
+printf("4\n");
+printf("5\n");
+*/
+
+    return 0;
+}
